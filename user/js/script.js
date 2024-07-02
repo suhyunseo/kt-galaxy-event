275214e758185d9c266552e87aecfe6afdd7520b
voice1996 <voice1996@naver.com>
2024-07-02 18:01:12 +0900

    240702

--- user/js/script.js
+++ user/js/script.js
@@ -34,3 +34,16 @@ $(document).ready(function () {
         }
     });
 });
+
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        console.log(target);
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000); // 시간은 1000밀리초로 설정 (1초)
+        }
+    });
+});
